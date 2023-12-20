3cf609c432488cf23b00b28e846b12177e432b83
M.Hammad <hammmadakhter@gmail.com>
2023-12-20 18:17:54 +0500

    create todos list item and saved on local storage
    create todos list item and saved on local storage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import {AiOutlineDelete} from "react-icons/ai";
 import {BsCheckLg} from "react-icons/bs";
 
 function App() {
   const [iscompletescreen,setiscompletescreen] = useState(false);
+  const [allTodos,setTodos]= useState([]);
+  const [newstudents,setnewstudents]= useState("");
+  const [newdetail,setnewdetail] = useState("");
+
+  const handleAddTodo = ()=>{
+    let newTodoItem = {
+      student:newstudents,
+      detail:newdetail
+    }
+
+    let updatedTodoArr = [...allTodos];
+    updatedTodoArr.push(newTodoItem);
+    setTodos(updatedTodoArr);
+    localStorage.setItem('todolist',JSON.stringify(updatedTodoArr));
+  }
+
+    useEffect(()=>{
+      let savedtodo= JSON.parse(localStorage.getItem('todolist'));
+      if(savedtodo){
+        setTodos(savedtodo);
+      }
+    },[])
+
   return (
     <div className="App">
-      <h1 className="my-3">MY Todos</h1>
+      <h1 className="my-3">Students attendance Data</h1>
 
       <div className="todo-wrapper">
         <div className="todo-input">
           <div className="todo-input-item">
             <label>Student Name</label>
-            <input type="text" placeholder="Enter student Name" />
+            <input type="text" value={newstudents} onChange={(e)=>setnewstudents(e.target.value)} placeholder="Enter student Name" />
           </div>
           <div className="todo-input-item">
             <label>Student Detail</label>
-            <input type="text" placeholder="Enter student detail" />
+            <input type="text" value={newdetail} onChange={(e)=>setnewdetail(e.target.value)} placeholder="Enter student detail" />
           </div>
           <div className="todo-input-item">
-            <button type="button" className="primary-Btn">
+            <button type="button" onClick={handleAddTodo} className="primary-Btn">
               Add
             </button>
           </div>
@@ -32,20 +55,24 @@ function App() {
       </div>
 
       <div className="todo-list">
-      <div className="todo-list-item">
-        <div>
-         <h3>student name 1</h3>
-          <p>Student Detail</p>
+      {allTodos.map((item,index)=>{
+        return(
+          <div className="todo-list-item" key={index}>
+          <div>
+           <h3>{item.student}</h3>
+            <p>{item.detail}</p>
+          </div>
+          <div>
+          <AiOutlineDelete
+                      className="icon"
+                    />
+                    <BsCheckLg
+                      className=" check-icon"
+                    />
+          </div>
         </div>
-        <div>
-        <AiOutlineDelete
-                    className="icon"
-                  />
-                  <BsCheckLg
-                    className=" check-icon"
-                  />
-        </div>
-      </div>
+        )
+      })}
       </div>
 
       
